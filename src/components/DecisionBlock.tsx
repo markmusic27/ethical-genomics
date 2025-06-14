365dc1c52ff69bec5f90a879cb6a82e7707a4347
Mark Music <me@markmusic.io>
2025-06-14 01:50:24 -0600

    ft: finish annotated bib and compress photos

--- src/components/DecisionBlock.tsx
+++ src/components/DecisionBlock.tsx
@@ -21,7 +21,7 @@ const DecisionBlock = ({
       onClick={handleClick}
       className="transition:scale flex h-[350px] w-full cursor-pointer flex-col rounded-[6px] bg-cover bg-center p-[12px] duration-200 hover:scale-101"
       style={{
-        backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3) 10%, rgba(0,0,0,0) 40%, rgba(0,0,0,0) 60%, rgba(0,0,0,0.6) 90%), url('/game/${block.id}.png')`,
+        backgroundImage: `linear-gradient(to bottom, rgba(0,0,0,0.3) 10%, rgba(0,0,0,0) 40%, rgba(0,0,0,0) 60%, rgba(0,0,0,0.6) 90%), url('/game/${block.id}-min.png')`,
       }}
     >
       <div className="flex h-full w-full flex-col justify-between">
