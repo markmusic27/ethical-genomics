170967e0342f3163ab33eac87485cea64561d0a5
Mark Music <me@markmusic.io>
2025-06-14 01:56:55 -0600

    fix: try to fix app and see if it worked

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default tseslint.config(
       ...tseslint.configs.stylisticTypeChecked,
     ],
     rules: {
+      "react/no-unescaped-entities": 0,
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
